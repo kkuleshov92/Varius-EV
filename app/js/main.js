99fbf2e7853eb0f1281144bbf3771a98ecdf62a8
kkuleshov92 <nstkuleshov1@gmail.com>
2019-05-15 17:14:07 +0300

    first commit

---
+++ app/js/main.js
@@ -0,0 +1,10 @@
+
+//=include ../components/header/header.js
+//=include ../components/progress/progress.js
+//=include ../components/help/help.js
+//=include ../components/consist/consist.js
+//=include ../components/usage/usage.js
+//=include ../components/reviews/reviews.js
+//=include ../components/order/order.js
+//=include ../components/last/last.js
+//=include ../components/footer/footer.js
