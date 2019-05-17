d40b85af25a1ff0b37ba90ad6c92a859fffd3fa1
ValentynKvasnytskyi <kvasnitskiy.v@gmail.com>
2019-05-17 13:04:52 +0300

    help adaptive

--- app/components/help/help.js
+++ app/components/help/help.js
@@ -5,6 +5,7 @@ $('.help__left-slider').slick({
     asNavFor: '.help__right-slider',
     dots: true,
     arrows: true,
+    adaptiveHeight: true,
     prevArrow: '<svg xmlns="http://www.w3.org/2000/svg" class="help__prev" width="55" height="48" viewBox="0 0 55 48"><g><g><path fill="#ccc4b3" d="M0 23h.548L21.435.154l1.414 1.412L3.255 23H55v2H3.257l19.592 21.434-1.414 1.415L.549 25.001H0z"/></g></g></svg>',
     nextArrow: '<svg xmlns="http://www.w3.org/2000/svg" class="help__next" width="55" height="48" viewBox="0 0 55 48"><g><g><path fill="#ccc4b3" d="M0 23h.548L21.435.154l1.414 1.412L3.255 23H55v2H3.257l19.592 21.434-1.414 1.415L.549 25.001H0z"/></g></g></svg>'
 });
