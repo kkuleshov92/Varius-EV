3fb503dcb907dc16cd715a822da03c7c0accf4f7
kkuleshov92 <nstkuleshov1@gmail.com>
2019-05-31 13:23:56 +0300

    edit

--- app/components/footer/footer.js
+++ app/components/footer/footer.js
@@ -1,9 +1,7 @@
 $(window).scroll(function () {
     if ($(this).scrollTop() > 600) {
         $('.go-top-btn').css('display','block');
-        console.log('more');
     } else {
         $('.go-top-btn').css('display','none');
-        console.log('less');
     }
 });
